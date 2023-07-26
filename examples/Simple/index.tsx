c2e3dda2adf3b194fc839c708ea8975724d0f113
natalyst <nattapong.anuwong@gmail.com>
2023-07-26 10:41:46 +0700

    chore: adding more test cases to use-shard

--- examples/Simple/index.tsx
+++ examples/Simple/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useShard } from "../../src";
 import { counterShard } from "./shard.constant";
 import Child from "./Child";
 
+const Hidden = () => {
+  const [counter] = useShard(counterShard);
+  return <div data-testid="hidden-element">{counter}</div>;
+};
+
 const Simple = () => {
   const [counter, setCounter] = useShard(counterShard);
+  const [show, setShow] = useState(false);
+  const [double, setDouble] = useState(0);
+
+  useEffect(() => {
+    setDouble(counter * 2);
+  }, [counter]);
+
   return (
     <>
       <div data-testid="simple-wrapper">
@@ -22,6 +34,11 @@ const Simple = () => {
           -1
         </button>
       </div>
+      <div data-testid="use-effect-sync">{double}</div>
+      <button data-testid="show-button" onClick={() => setShow(true)}>
+        show
+      </button>
+      {show && <Hidden />}
       <Child />
     </>
   );
