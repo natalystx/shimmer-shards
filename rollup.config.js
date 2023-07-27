28859ba6e74e185ac909dc44ae40c84e8f48104d
Nattapong Anuwong <41513810+qusenazlas@users.noreply.github.com>
2023-07-27 16:59:45 +0700

    Chore/add roll up (#21)
    Chore/add roll up

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({ sourceMap: true, declaration: true }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+      }),
+      external(),
+      resolve(),
+      terser(),
+    ],
+  },
+];
