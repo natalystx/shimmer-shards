9593a3afd14416555aa9b69ac45066a8d62a8270
Nattapong Anuwong <41513810+natalystx@users.noreply.github.com>
2023-07-31 10:10:06 +0700

    chore: update head seo tags (#36)

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { Kanit } from "next/font/google";
+import Head from "next/head";
 
 const kanit = Kanit({
   weight: ["300", "500", "600", "700"],
