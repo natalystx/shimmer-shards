98678514a2db021c5247481bac8f8ac0a21c40af
Nattapong Anuwong <41513810+natalystx@users.noreply.github.com>
2023-08-03 16:30:06 +0700

    Bump: 0.2.0 (#40)

---
+++ src/core/cluster/index.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+
+import { PersistShard, usePersistShard } from "../persist";
+import { Shard, useShard } from "../shard";
+
+export type ClusterMembers<Type = {}> = {
+  [Property in keyof Type]: Shard<Type[Property]>;
+};
+
+export type PrevFn<Type> = (v: Type) => Type;
+
+export type State<Type> = [Type, (v: Type | PrevFn<Type>) => void];
+
+type ReturnClusterMembers<Type> = {
+  [Property in keyof Type]: () => State<Type[Property]>;
+};
+
+export const useCluster = <Type>(
+  data: ClusterMembers<Type>
+): ReturnClusterMembers<Type> => {
+  // eslint-disable-next-line prefer-const, @typescript-eslint/no-explicit-any
+  let states: any = {};
+  for (const item in data) {
+    if ((data[item] as any)?.getKey) {
+      states[item] = () => usePersistShard(data[item] as PersistShard<any>);
+    } else {
+      states[item] = () => useShard(data[item]);
+    }
+  }
+
+  return states as ReturnClusterMembers<Type>;
+};
