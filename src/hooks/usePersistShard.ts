79a0d3d56c138e2886dbaa395e47d7d4a5cd6ccb
Nattapong Anuwong <41513810+natalystx@users.noreply.github.com>
2023-08-04 12:36:37 +0700

    chore: cleanup code and types (#43)

---
+++ src/hooks/usePersistShard.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useState, useEffect } from "react";
+import { PersistShard } from "../core/persist";
+import Publisher from "../core/publisher";
+import { State, PrevFn } from "../types";
+
+const publishers = new Map<string, any>();
+
+export const usePersistShard = <Type>(
+  persistShard: PersistShard<Type>
+): State<Type> => {
+  const publisher = new Publisher<Type>();
+
+  let data = JSON.parse(localStorage.getItem(persistShard.getKey()) || "null");
+  const [state, setState] = useState(data || persistShard.getInitialValue());
+  const id = persistShard.getId();
+  useEffect(() => {
+    if (!id && persistShard.getKey()) return;
+    if (!publishers.has(id)) {
+      publishers.set(id, publisher);
+    }
+
+    if (data === null) {
+      localStorage.setItem(
+        persistShard.getKey(),
+        JSON.stringify(persistShard.getInitialValue())
+      );
+      data = persistShard.getInitialValue();
+    }
+
+    if (publishers.get(id)?.getRecentlyData()) {
+      setState(publishers.get(id)?.getRecentlyData());
+    } else {
+      setState(
+        data ?? persistShard.getInitialValue() ?? persistShard.getFallbackData()
+      );
+    }
+
+    const subscribe = publishers.get(id)?.subscribe((v: any) => {
+      setState(v);
+    });
+
+    return () => subscribe?.unsubscribe();
+  }, [id]);
+
+  return [
+    state as Type,
+    (v: Type | PrevFn<Type>): void => {
+      if (typeof v === "function") {
+        const newValue = (v as unknown as Function)(state) as Type;
+        localStorage.setItem(persistShard.getKey(), JSON.stringify(newValue));
+        publishers.get(id)?.publish(newValue);
+        return;
+      }
+
+      publishers.get(id)?.publish(v);
+      localStorage.setItem(persistShard.getKey(), JSON.stringify(v));
+    },
+  ];
+};
---
+++ src/hooks/useShard.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useState, useMemo, useEffect } from "react";
+import { useScope, scopedList } from "../components/Scope";
+import Publisher from "../core/publisher";
+import { Shard } from "../core/shard";
+import { State, PrevFn } from "../types";
+import { getId } from "../utils/getId";
+
+const publishers = new Map<string, Publisher<any>>();
+
+export const useShard = <Type>(shard: Shard<Type>): State<Type> => {
+  const [state, setState] = useState(shard.getInitialValue());
+  const publisher = new Publisher<Type>();
+  const { scopeId } = useScope();
+
+  const id = useMemo(() => getId(scopedList, shard, scopeId), [scopeId]);
+
+  useEffect(() => {
+    if (!id) return;
+    if (!publishers.has(id)) {
+      publishers.set(id, publisher);
+    }
+
+    if (publishers.get(id)?.getRecentlyData()) {
+      setState(publishers.get(id)?.getRecentlyData());
+    }
+    const subscribe = publishers.get(id)?.subscribe((v) => {
+      setState(v);
+    });
+
+    return () => subscribe?.unsubscribe();
+  }, [id]);
+
+  return [
+    state,
+    (v: Type | PrevFn<Type>): void => {
+      if (typeof v === "function") {
+        const newValue = (v as unknown as Function)(state) as Type;
+        publishers.get(id)?.publish(newValue);
+        return;
+      }
+
+      publishers.get(id)?.publish(v);
+    },
+  ];
+};
+
+export const useStoreValue = <Type>(shard: Shard<Type>): Type => {
+  const [state, setState] = useState(shard.getInitialValue());
+  const publisher = new Publisher<Type>();
+  const { scopeId } = useScope();
+  const id = useMemo(() => getId(scopedList, shard, scopeId), [scopeId]);
+
+  useEffect(() => {
+    if (!publishers.has(id)) {
+      publishers.set(id, publisher);
+    }
+    publishers.get(id)?.subscribe((v: Type) => {
+      setState(v);
+    });
+  }, []);
+
+  return state;
+};
