e491b8ef0b0efca1b112f37b523168af70d00877
Nattapong Anuwong <41513810+natalystx@users.noreply.github.com>
2023-09-04 08:48:03 +0700

    chore: update rollup bundling config (#46) (#47)

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import generatePackageJson from "rollup-plugin-generate-package-json";
 
 export default [
   {
@@ -10,12 +12,13 @@ export default [
     output: [
       {
         file: "dist/index.js",
+        format: "cjs",
         exports: "named",
         sourcemap: true,
       },
       {
         file: "dist/index.es.js",
-        format: "es",
+        format: "esm",
         exports: "named",
         sourcemap: true,
       },
@@ -26,9 +29,23 @@ export default [
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
       }),
-      external(),
+      commonjs({
+        include: ["node_modules/**"],
+      }),
       resolve(),
       terser(),
+      external(["react", "react-dom"]),
+      generatePackageJson({
+        outputFolder: "dist",
+        baseContents: (pkg) => ({
+          name: pkg.name,
+          main: pkg.main.replace("dist/", ""),
+          module: pkg.module.replace("dist/", ""),
+          dependencies: {
+            uuid: "^9.0.0",
+          },
+        }),
+      }),
     ],
   },
 ];
