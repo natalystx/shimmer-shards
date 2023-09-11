ecddf36722d965bceee036fd92558e0759006760
natalyst <nattapong.anuwong@gmail.com>
2023-09-11 13:08:21 +0700

    feat: implement effect remount fn call

--- tests/effect-with-scope.test.tsx
+++ tests/effect-with-scope.test.tsx
@@ -6,13 +6,12 @@ import MemoEffectScopeComponent from "../examples/MemoEffectWithScope";
 const fn = jest.fn();
 const fn2 = jest.fn();
 const fn3 = jest.fn();
+let component: any;
 beforeEach(() => {
-  fn.mockClear();
-  fn2.mockClear();
-  fn3.mockClear();
-  render(<MemoEffectScopeComponent fn={fn} fn2={fn2} fn3={fn3} />);
+  component = render(<MemoEffectScopeComponent fn={fn} fn2={fn2} fn3={fn3} />);
 });
 afterEach(() => {
+  component.unmount();
   fn.mockClear();
   fn2.mockClear();
   fn3.mockClear();
@@ -35,7 +34,7 @@ describe("effect", () => {
     fireEvent.click(button);
     expect(fn).toBeCalledTimes(3);
     expect(fn2).toBeCalledTimes(2);
-    expect(fn3).toBeCalledTimes(0);
+    expect(fn3).toBeCalledTimes(1);
     expect(screen.getByTestId("counter-text-A-scope").innerHTML).toEqual("1");
     expect(screen.getByTestId("counter-text-outside-scope").innerHTML).toEqual(
       "0"
@@ -47,6 +46,6 @@ describe("effect", () => {
     );
     expect(fn).toBeCalledTimes(3);
     expect(fn2).toBeCalledTimes(2);
-    expect(fn3).toBeCalledTimes(1);
+    expect(fn3).toBeCalledTimes(2);
   });
 });
